9c5b427c4b4609a874d862be07b4af2d3388bf3c
Parth <parth@caretit.com>
2022-01-05 16:33:42 +0530

    [ADD] quality control customization

---
+++ quality_control_customization/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Part of Aardug. (Website: www.aardug.nl).
+# See LICENSE file for full copyright and licensing details.
+#
+##############################################################################
+
+{
+    'name': "Quality control customization",
+    'summary': """Quality control customization""",
+    'description': """
+            1) Changed Warning Message based on measure success in Quality Check
+    """,
+    'author': "Aardug, Arjan Rosman",
+    'website': "http://www.aardug.nl/",
+    'support': 'arosman@aardug.nl',
+    'version': '15.0.0.1',
+    'category': 'Account',
+    'depends': ['quality_control'],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
