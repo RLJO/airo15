8e0abf75d0b84259fe215e2c2317476038b12106
Parth <parth@caretit.com>
2022-01-05 15:42:13 +0530

    [RMV] modules

--- ct_footer_debranding/doc/changelog.rst
+++
@@ -1,8 +0,0 @@
-.. _changelog:
-
-Changelog
-=========
-
-`Version 1.0 (07-12-2021)`
--------------------------
-- First release for Odoo 15.0.
--- oob/models/oobapi/__init__.py
+++
@@ -1,5 +0,0 @@
-# import oobapi
-from .oobapi import OpencartWebService
-from .oobapi import OpencartWebServiceDict
-# from oobapi import OpencartWebServiceError
-# from oobapi import PrestaShopAuthenticationError
