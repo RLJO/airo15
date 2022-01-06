534d08aba85f2dd19d87eb4a0a9ab5bf03dcbb59
Parth <parth@caretit.com>
2022-01-06 11:35:13 +0530

    [ADD] some modules

---
+++ google_shop/models/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##########################################################################
+# Author : Webkul Software Pvt. Ltd. (<https://webkul.com/>;)
+# Copyright(c): 2017-Present Webkul Software Pvt. Ltd.
+# All Rights Reserved.
+#
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#
+# You should have received a copy of the License along with this program.
+# If not, see <https://store.webkul.com/license.html/>;
+##########################################################################
+
+from . import oauth2_detail
+from . import google_shop
+from . import google_fields
+from . import field_mapping
+from . import field_mapping_line
+from . import product_mapping
+from . import product
+from . import res_config_settings
